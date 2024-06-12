29b93d14d2b3fca594d76bbaeb635e6a791228bd
Felipe Pires <pires.felipe@ifood.com.br>
2024-06-12 19:54:46 -0300

    Spring Initializr initial config

---
+++ backend/src/test/kotlin/com/unicamp/rottenavocados/RottenavocadosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unicamp.rottenavocados
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RottenavocadosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
