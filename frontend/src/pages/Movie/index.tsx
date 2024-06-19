cd33bb343bbb57d6ddc9b122161e2151ee6c9008
Artur Oliveira <artur.oliveira@ifood.com.br>
2024-06-18 22:01:36 -0300

    add movie and serie page

--- frontend/src/pages/Movie/index.tsx
+++ frontend/src/pages/Movie/index.tsx
@@ -1,5 +1,36 @@
-const Movie: React.FC = () => {
-	return <h1>Movie</h1>;
+import { useNavigate, useParams } from "react-router-dom";
+import { movies } from "../../mock";
+
+import * as Styled from "./styles";
+
+const formatDate = (date?: string) => {
+	const args = date?.split("-");
+	return args?.[2] + "/" + args?.[1] + "/" + args?.[0];
 };
 
-export default Movie;
+const MoviePage: React.FC = () => {
+	const id = useParams().id;
+	const navigate = useNavigate();
+
+	const movie = movies.find((m) => m.id === id);
+
+	if (!movie) {
+		navigate("/");
+	}
+
+	return (
+		<Styled.Container>
+			<h1>{movie?.name}</h1>
+			<p>Gênero: {movie?.genre.name}</p>
+			<p>{movie?.duration} minutos</p>
+			<p>Nota {movie?.grade}</p>
+			<p>Lançado em {formatDate(movie?.releaseDate)}</p>
+
+			<Styled.Reviews>
+				<h1>Availiações</h1>
+			</Styled.Reviews>
+		</Styled.Container>
+	);
+};
+
+export default MoviePage;
---
+++ frontend/src/pages/Serie/index.tsx
@@ -0,0 +1,39 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { series } from "../../mock";
+
+import * as Styled from "./styles";
+
+const formatDate = (date?: string) => {
+	const args = date?.split("-");
+	return args?.[2] + "/" + args?.[1] + "/" + args?.[0];
+};
+
+const SeriePage: React.FC = () => {
+	const id = useParams().id;
+	const navigate = useNavigate();
+
+	const serie = series.find((m) => m.id === id);
+
+	if (!serie) {
+		navigate("/");
+	}
+
+	return (
+		<Styled.Container>
+			<h1>{serie?.name}</h1>
+			<p>Gênero: {serie?.genre.name}</p>
+			<p>
+				{serie?.seasons} temporada{serie?.seasons && serie?.seasons > 1 && "s"}
+			</p>
+			<p>{serie?.episodes} episódio{serie?.episodes && serie?.episodes > 1 && "s"}</p>
+			<p>Nota {serie?.grade}</p>
+			<p>Lançado em {formatDate(serie?.releaseDate)}</p>
+
+			<Styled.Reviews>
+				<h1>Availiações</h1>
+			</Styled.Reviews>
+		</Styled.Container>
+	);
+};
+
+export default SeriePage;
