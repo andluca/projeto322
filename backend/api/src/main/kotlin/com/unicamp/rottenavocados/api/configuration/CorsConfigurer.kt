e60a8eafb852a9dc9fcd56ce91d3dcb0ebaf5226
Felipe Pires <pires.felipe@ifood.com.br>
2024-06-23 20:59:25 -0300

    Configure CORS

---
+++ backend/api/src/main/kotlin/com/unicamp/rottenavocados/api/configuration/CorsConfigurer.kt
@@ -0,0 +1,19 @@
+package com.unicamp.rottenavocados.api.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfigurer {
+    @Bean
+    fun corsConfigurer() =
+        object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+}
