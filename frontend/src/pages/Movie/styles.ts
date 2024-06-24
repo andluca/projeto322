1cc111346943f932134fef78c478f2a9eb5b0bb6
andluca <109103844+andluca@users.noreply.github.com>
2024-06-24 03:51:37 -0300

    feat: adiciona estilização à visualização de filmes

--- frontend/src/pages/Movie/styles.ts
+++ frontend/src/pages/Movie/styles.ts
@@ -3,12 +3,23 @@ import styled from "styled-components";
 export const Container = styled.div`
   margin: 40px 0;
   padding: 0 80px;
+  color: #fff;
 
+  .picture{
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  .picture img{
+    max-width: 300px;
+  }
   p {
     font-size: 18px;
   }
 `;
 
 export const Reviews = styled.div`
-  margin-top: 80px;
+  margin-top: 20px;
 `;
