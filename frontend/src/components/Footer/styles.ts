cf275043597ae715abd79e5f56282e2442790e59
andluca <109103844+andluca@users.noreply.github.com>
2024-06-24 03:25:18 -0300

    feat: adiciona footer ao projeto

---
+++ frontend/src/components/Footer/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  width: 100%;
+  padding: 30px 80px;
+  background: #024201;
+  align-items: center;
+
+  h1 {
+    font-weight: 500;
+    color: #fff;
+    font-size: 25px;
+  }
+
+  p{
+    padding-top: 30px;
+    padding-bottom: 30px;
+    font-weight: 500;
+    color: #fff;
+    font-size: 16px;
+  }
+
+  div > p{
+    color: #fff;
+    padding-top: 0px;
+    padding-bottom: 0px;
+  }
+
+`;
